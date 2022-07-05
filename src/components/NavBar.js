c061bb556f0f21a401c5adfb8f1eadcbab553dd1
LeandroSanchez <sanchezk.leandro@gmail.com>
2022-07-04 23:30:06 -0300

    Agregado NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo2 from '../logo2.webp'
+
+
+const NavBar = () => {
+    return (
+        <Navbar bg="warning" variant="light">
+        <Container>
+          <Navbar.Brand href="#home" >
+          <img
+              src={logo2}
+              width="200"
+              height="60" 
+              className="d-inline-block align-top" 
+              alt="logo"
+            />
+          </Navbar.Brand>
+          <Nav className="justify-content-end">
+            <Nav.Link href="#home" className='active'>Inicio</Nav.Link> 
+            <Nav.Link href="#features" className='active'>Productos</Nav.Link>
+            <Nav.Link href="#pricing" className='active'>Nosotros</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  
+    );
+}
+export default NavBar;
