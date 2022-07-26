14045159b4d716adf5562886f2a6545cf62641ed
LeandroSanchez <sanchezk.leandro@gmail.com>
2022-07-25 22:32:07 -0300

    sincronizado ItemCount

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { Button } from "react-bootstrap";
 
-const ItemCount = ({ stock }) => {
+const ItemCount = ({ stock , onAdd}) => {
     const [count, setCount] = useState(0);
-
+   
     const updateCount = (operation) => {
         if (operation === "-" && count > 0) {
             setCount(count - 1);
@@ -11,20 +11,21 @@ const ItemCount = ({ stock }) => {
             setCount(count + 1);
         }
     };
-    const addCart = () => {
-        alert("Se agregaron " + count + " al carrito");
-    }
+    
     return (
         <>
-            <div className=" input-group" >
-                <button type="button" className="btn btn-secondary" onClick={() => updateCount("-")}>-</button>
-                <input value={count} style={{ width: "3rem" }} className="text-center" onChange={updateCount} ></input>
-                <button type="button" className="btn btn-secondary" onClick={() => updateCount("+")}>+</button>
-            </div>
-            <div style={{ marginTop: "5px" }}>
-                <button onClick={() => addCart()} type="button" className="btn btn-primary" style={{ width: "100%", marginTop: "10px" }} disabled={count<1}>Agregar al carrito!</button>
-
-            </div>
+            
+                <div>
+                    <div className=" input-group" >
+                        <button type="button" className="btn btn-secondary" onClick={() => updateCount("-")}>-</button>
+                        <input value={count} style={{ width: "3rem" }} className="text-center" onChange={updateCount} ></input>
+                        <button type="button" className="btn btn-secondary" onClick={() => updateCount("+")}>+</button>
+                    </div>
+                    <div style={{ marginTop: "5px" }}>
+                        <button onClick={() => onAdd(count)} type="button" className="btn btn-primary" style={{ width: "100%", marginTop: "10px" }} disabled={count < 1}>Agregar al carrito!</button>
+                    </div>
+                </div>
+            
 
 
         </>
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,36 +1,55 @@
 import { useState } from "react";
 import ItemCount from "./ItemCount";
+import { Link } from "react-router-dom";
 
 const ItemDetail = ({ item }) => {
-    const { title, price, stock, image , category , description} = item;
+    const { title, price, stock, image, category, description } = item;
+    const [amount, setAmount] = useState(0);
+    const onAdd = (amount) => {
+        setAmount(amount);
+        alert("Se agregaron " + amount + " al carrito");
+    };
     return (
         <div className="container">
             <div className="row d-flex justify-content-center">
-                <div className="col-sm-4 " style={{borderRight:"1px" ,borderRightStyle:"solid" ,borderRightColor:"#f6f6f6"}}>
-                    <img style={{ width: "100%", height:"25rem"}} src={image} />
+                <div className="col-sm-4 " style={{ borderRight: "1px", borderRightStyle: "solid", borderRightColor: "#f6f6f6" }}>
+                    <img style={{ width: "100%", height: "25rem" }} src={image} />
                 </div>
-                <div className="col-sm-5" style={{paddingTop:"20px"}} >
+                <div className="col-sm-5" style={{ paddingTop: "20px" }} >
 
                     <h2 >{title}</h2>
                     <h5> {category} </h5>
 
-                    <h6 className="title-price" style={{marginTop:"40px", marginBottom:"0px"}}><small>PRECIO</small></h6>
-                    <h3 style={{marginTop:"0px"}}>$ {price}</h3>
-                    <div className="section " style={{ background:"#f8f9f9", marginLeft:"-12px",paddingLeft:"15px", paddingRight:"15px", paddingBottom:"30px", paddingTop:"20px"}}>
+                    <h6 className="title-price" style={{ marginTop: "40px", marginBottom: "0px" }}><small>PRECIO</small></h6>
+                    <h3 style={{ marginTop: "0px" }}>$ {price}</h3>
+                    <div className="section " style={{ background: "#f8f9f9", marginLeft: "-12px", paddingLeft: "15px", paddingRight: "15px", paddingBottom: "30px", paddingTop: "20px" }}>
                         <h6 className="title-attr"><small>CANTIDAD</small></h6>
                         <p className="">{`${stock} unidades disponibles`}</p>
-                        <ItemCount stock={stock}/>
+                        {amount === 0 ? (
+                            <ItemCount stock={stock} onAdd={onAdd}/>
+                        ) : (
+                            <div>
+                                <h2> Se agregaron {amount} articulo(s)</h2>
+                                <div style={{ marginTop: "5px" }}>
+                                    <Link to="/cart">
+                                        <button type="button" className="btn btn-primary" style={{ width: "100%", marginTop: "10px" }} >Comprar</button>
+                                    </Link>
+                                </div>
+                            </div>
+
+                        )}
+
                     </div>
                 </div>
                 <div className="col-sm-10 ">
-                    <ul className="menu-items" style={{listStyleType:"none",fontSize:"20px", display:"inline-flex", marginBottom:"0px", marginTop:"20px"}}>
+                    <ul className="menu-items" style={{ listStyleType: "none", fontSize: "20px", display: "inline-flex", marginBottom: "0px", marginTop: "20px" }}>
                         <li className="active">Descripcion del producto</li>
                     </ul>
-                    <div style={{width:"100%", borderTop:"1px", borderTopStyle:"solid", borderTopColor:"silver"}}>
-                        <p style={{padding:"15px"}} >
+                    <div style={{ width: "100%", borderTop: "1px", borderTopStyle: "solid", borderTopColor: "silver" }}>
+                        <p style={{ padding: "15px" }} >
                             {description}
                         </p>
-                        
+
                     </div>
                 </div>
             </div>
